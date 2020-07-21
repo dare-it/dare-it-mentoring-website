fef1e284b42a2d94e09c71656762274b1857d1dc
Natalie Pilling <nataliepilling@Natalies-MacBook-Pro.local>
2020-07-21 21:11:58 +0200

    Add GA site tag

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -22,11 +22,10 @@ export default (props) => {
   return (
     <html>
       <head>
-        <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-168093382-3"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          function gtag(){dataLayer.push(arguments)}
           gtag('js', new Date());
         gtag('config', 'UA-168093382-3');
         </script>
