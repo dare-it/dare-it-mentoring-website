942666ddd2383c210dbda3c7b7d4cc03703416b2
Natalie Pilling <nataliepilling@Natalies-MacBook-Pro.local>
2020-12-02 10:11:45 +0100

    Update Privacy Page

--- src/components/translationsPersonalData.js
+++ src/components/translationsPersonalData.js
@@ -9,7 +9,7 @@ const translationsPersonalData = {
     [
       {
         type: 'paragraph',
-        content: 'Administratorem Twoich danych osobowych jest Aleksandra Bis, reprezentująca program mentoringowy dareIT. Twoje dane będziemy przechowywać i przetwarzać na podstawie Twojej zgody w celu:'
+        content: 'Administratorem Twoich danych osobowych jest Aleksandra Bis, reprezentująca Dare IT (DareIT Sp. z o.o.). Twoje dane będziemy przechowywać i przetwarzać na podstawie Twojej zgody w celu:'
       },
       {
         type: 'ordered-list',
@@ -17,6 +17,7 @@ const translationsPersonalData = {
           'naboru uczestników do programu mentoringowego Dare IT;',
           'przeprowadzenia bieżącej edycji programu;',
           'przeprowadzenia przyszłych edycji programu;',
+          'wysyłać Ci Newsletter Dare IT;',
           'informowania Cię o bieżących inicjatywach Dare IT.'
         ]
       }
@@ -24,7 +25,7 @@ const translationsPersonalData = {
     [
       {
         type: 'paragraph',
-        content: 'Twoje dane pozostaną w naszym posiadaniu przez okres 12 miesięcy.'
+        content: 'Twoje dane są przechowywane w naszej bazie danych kontaktów na czas nieokreślony. Zawsze możesz zażądać ich usunięcia poprzez privacy@dareit.org.'
       }
     ],
     [
@@ -34,20 +35,16 @@ const translationsPersonalData = {
       },
       {
         type: 'paragraph',
-        content: 'W każdej chwili przysługuje Ci prawo wniesienia sprzeciwu wobec przetwarzania Twoich danych – możesz tego dokonać poprzez przesłanie wiadomości e-mail na adres aleksandra.bis@dareit.org.'
+        content: 'W każdej chwili przysługuje Ci prawo wniesienia sprzeciwu wobec przetwarzania Twoich danych – możesz tego dokonać poprzez przesłanie wiadomości e-mail na adres privacy@dareit.org.'
       },
       {
         type: 'paragraph',
         content: 'Twoje dane będą usuwane w przypadku wycofania przez Ciebie zgody na ich przetwarzanie, wniesienia sprzeciwu wobec przetwarzania lub gdy ustalimy, że Twoje dane się zdezaktualizowały, bądź nie są nam już potrzebne do celów, dla których zostały zebrane (np.: gdy przestaniesz wyrażać chęć udziału w naszych przyszłych edycjach).'
-      }
-    ],
-    [
+      },
       {
         type: 'paragraph',
         content: 'Dare IT nie udostępnia Twoich danych innym podmiotom, choć w pewnych okolicznościach możemy być zobowiązani do przekazania Twoich danych właściwym organom (w szczególności policji lub prokuraturze), jeżeli zostanie do nas skierowane takie żądanie, o ile istnieje podstawa prawna udostępnienia takich danych (np.: nakaz sądowy).'
-      }
-    ],
-    [
+      },
       {
         type: 'paragraph',
         content: 'Ponadto informujemy, że masz prawo do:'
@@ -60,7 +57,11 @@ const translationsPersonalData = {
           'wniesienia sprzeciwu wobec przetwarzania danych, w zakresie w jakim jest ono oparte na prawnie uzasadnionym interesie;',
           'wniesienia skargi do organu nadzoru.'
         ]
-      }
+      },
+      {
+        type: 'paragraph',
+        content: 'Aby zamówić którekolwiek z powyższych rozwiązań skontaktuj się z nami pod adresem privacy@dareit.org. Jesteśmy zobowiązani do wykonania Twoich praw.'
+      },
     ]
   ]
 };
